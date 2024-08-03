bca0b67a10d017d3e273dbc9d861ae31aa41e1a9
Kumar Shantanu <thecrazyphoton@gmail.com>
2024-08-04 02:47:44 +0530

    Setup ESlint and Prettier configurations

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import pluginJs from "@eslint/js";
+import jest from "eslint-plugin-jest";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+  { ignores: ["dist/"] },
+  { files: ["src:/**/*.{js,ts,jsx,tsx}", "tests/**/*.{js,ts,jsx,tsx}"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["tests/**/*.{js,ts,jsx,tsx}"],
+    ...jest.configs["flat/recommended"],
+    rules: {
+      ...jest.configs["flat/recommended"].rules,
+      "jest/prefer-expect-assertions": "off",
+    },
+  },
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
+  eslintPluginPrettierRecommended,
+];
+
+//npx eslint . --fix
